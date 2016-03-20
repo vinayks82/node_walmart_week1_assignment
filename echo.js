c4ecb6e01bf6b5cd10c3901cdea2d6af150886cf
vinay shankar <vshankar@walmartlabs.com>
2016-03-19 23:20:10 -0700

    changes

--- echo.js
+++ echo.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
-require('./helper')
-let fs = require('fs').promise
-
 function* echo() {
-    process.argv.forEach(function (param,position) {
-      if(position > 1) {
-        process.stdout.write(`${param}`)
-        if(position < process.argv.length) {
-          process.stdout.write(` `)
-        }
-      }
-    })
-    process.stdout.write(`\n`)
+    process.argv.forEach((param) => {
+      process.stdout.write(`${param}`);
+    });
 }
 
 module.exports = echo
