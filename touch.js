b28eb7b57fca15b6587ab80f2fd16d7ebcd983a3
vinay shankar <vshankar@walmartlabs.com>
2016-03-20 02:56:05 -0700

    fix errors

--- touch.js
+++ touch.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 "use strict";
 require('./helper')
-let fs = require('fs');
+let fs = require('fs')
+let path = require('path')
 
 function* touch() {
   touchFile(process.argv[2]);
 }
 
 function touchFile(fileName) {
-  fs.futimes(fileName, new Date(), new Date(), function(err, contents){
-
+  fs.open(path.join(process.cwd(), fileName), 'a', function(err, fd){
+    fs.futimes(fd, new Date(), new Date(), function(err, fd) {
+      if(err){
+        console.log(err);
+      }
+    })
   });
 }
 
