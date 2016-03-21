dd9669c5d131088945cd585546071af45b1c1453
vinay shankar <vshankar@walmartlabs.com>
2016-03-20 18:20:53 -0700

    adding rm

--- rm.js
+++ rm.js
@@ -6,27 +6,90 @@ let fs = require("fs"),
 path = require("path");
 
 function* rm() {
-  deleteFile(process.argv[2]);
+  let arg = process.argv[2].replace('./','');
+  removeDirectory(arg);
 }
 
-function deleteFile(fileName) {
-  fs.readdir(fileName, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-      console.log(items[i])
-      fs.unlink(items[i], function (err) {
-         if (err) {
-           console.log('failed to remove directory', err);
-         }
-      })
+function removeDirectory(dir) {
+  fs.lstat(dir, function(err, stats) {
+    if(err){
+      console.log(JSON.stringify(err));
+    }else{
+      if(stats.isFile()) {
+        fs.unlink(dir, function (err) {
+          if(err){
+            console.log(JSON.stringify(err));
+          }
+        })
+      }else {
+        //delete all the files in the directory
+        //then delete directory
+        removeDirectoryRecursive(dir);
+      }
     }
-    if(items.length < 1) {
-      fs.rmdir(fileName, function(err) {
-        if (err) {
-          console.log('failed to remove directory', err);
-        }
-      })
+  })
+}
+
+
+
+function removeDirectoryRecursive(dirPath) {
+  console.log("dddddd",dirPath);
+  fs.readdir(dirPath, function(err, files) {
+    if (err) {
+      console.log(JSON.stringify(err));
+    }else {
+      if(files.length === 0) {
+        fs.rmdir(dirPath, (err) => {
+            console.log("here2");
+          if(err) {
+            console.log(JSON.stringify(err));
+          }
+        })
+      }else {
+        files.forEach((file, index, files) => {
+          var filePath = dirPath +  file;
+          fs.stat(filePath, (err, stats) => {
+            if(err) {
+                console.log(JSON.stringify(err));
+            }else {
+              if (stats.isFile()) {
+               fs.unlink(filePath, function(err) {
+                   if (err) {
+                     console.log(JSON.stringify(err));
+                   }
+               });
+             }
+            }
+          })
+        })
+        console.log("DIRPATH",dirPath);
+        removeDirectory(dirPath);
+      }
+
+      // }else {
+      //   items.forEach((value, index, files) => {
+      //     var filePath = dirPath + file;
+      //     fs.stat(filePath, function(err, stats) {
+      //         if (err) {
+  	  //              console.log(JSON.stringify(err));
+  	  //         }else {
+      //           if (stats.isFile()) {
+  		//                 fs.unlink(filePath, function(err) {
+  		//                     if (err) {
+      //                       console.log(JSON.stringify(err));
+  		//                     }
+  		//                 });
+  	  //           }
+      //
+  	  //         if (stats.isDirectory()) {
+  		//             removeDirectoryRecursive(filePath + '/');
+  	  //            }
+      //         }
+      //     })
+      //   })
+      // }
     }
-  });
+   });
 }
 
 module.exports = rm
